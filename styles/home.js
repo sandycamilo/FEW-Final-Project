6093e6b7dcd8c7e606238650acc2989fea10e939
sandycamilo <scam499@lasierra.edu>
2019-12-14 04:48:53 +0200

    results

--- styles/home.js
+++ styles/home.js
@@ -137,7 +137,7 @@
         }
     });
 
-    resultsContainer.innerHTML = numCorrect + 'out of' + myQuestions.length;
+    resultsContainer.innerHTML = numCorrect + 'out of' + myQuestions.length + 'correct!';
  }
 
 
